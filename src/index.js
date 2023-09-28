96022624eeb8f1bccf8ef482f3dd63ab5f0e37e9
Banshidhar Pankaj <129153654+vipankaj006@users.noreply.github.com>
2023-09-29 01:26:38 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs').promises;
+const fileName = 'myfile.txt';
+
+const updateFile = async (fileName, fileContent) => {
+    //Write your code here to overwrite the file content
+    //Don't change function name
+    
+};
+
+
+module.exports = updateFile;
