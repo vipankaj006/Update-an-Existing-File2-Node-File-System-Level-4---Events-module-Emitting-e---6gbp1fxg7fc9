3bdb8d404c3a1badbeb8d99c3f294250f4dd3ac7
Banshidhar Pankaj <129153654+vipankaj006@users.noreply.github.com>
2023-09-28 20:06:37 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const fs = require('fs').promises;
-const fileName = 'myfile.txt';
+const fs = require("fs").promises;
+const fileName = "myfile.txt";
+const fileContent = ", is an online learning platform.";
 
 const updateFile = async (fileName, fileContent) => {
-    //Write your code here to overwrite the file content
-    //Don't change function name
-    
+  //Write your code here to overwrite the file content
+  //Don't change function name
+  fs.appendFileSync(fileName, fileContent);
 };
-
 
 module.exports = updateFile;
